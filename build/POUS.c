c254cfdebeb1812cf4ed869f1265145a639f2756
99sp4rky <brianamcc07@gmail.com>
2025-05-15 17:06:19 -0400

    ESP Counter Configuring
    Currently at the state of counting with ESP and measuring it on openPLC first.
    
    Next Step: After this is done we will try to communicate via wifi

---
+++ build/POUS.c
@@ -0,0 +1,91 @@
+void LOGGER_init__(LOGGER *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->MSG,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->LEVEL,LOGLEVEL__INFO,retain)
+  __INIT_VAR(data__->TRIG0,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void LOGGER_body__(LOGGER *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  if ((__GET_VAR(data__->TRIG,) && !(__GET_VAR(data__->TRIG0,)))) {
+    #define GetFbVar(var,...) __GET_VAR(data__->var,__VA_ARGS__)
+    #define SetFbVar(var,val,...) __SET_VAR(data__->,var,__VA_ARGS__,val)
+
+   LogMessage(GetFbVar(LEVEL),(char*)GetFbVar(MSG, .body),GetFbVar(MSG, .len));
+  
+    #undef GetFbVar
+    #undef SetFbVar
+;
+  };
+  __SET_VAR(data__->,TRIG0,,__GET_VAR(data__->TRIG,));
+
+  goto __end;
+
+__end:
+  return;
+} // LOGGER_body__() 
+
+
+
+
+
+void WATERESP_init__(WATERESP *data__, BOOL retain) {
+  __INIT_VAR(data__->PULSE,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->COUNTER_ENABLE,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->COUNTER_RESET,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->COUNTER_Q,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->COUNTER_MAX,10,retain)
+  CTU_init__(&data__->CTU0,retain);
+  __INIT_VAR(data__->COUNTER_VALUE,0,retain)
+  TON_init__(&data__->TON1,retain);
+  TOF_init__(&data__->TOF0,retain);
+  R_TRIG_init__(&data__->R_TRIG1,retain);
+}
+
+// Code part
+void WATERESP_body__(WATERESP *data__) {
+  // Initialise TEMP variables
+
+  __SET_VAR(data__->R_TRIG1.,CLK,,(__GET_VAR(data__->PULSE,) && __GET_VAR(data__->COUNTER_ENABLE,)));
+  R_TRIG_body__(&data__->R_TRIG1);
+  __SET_VAR(data__->CTU0.,EN,,__BOOL_LITERAL(TRUE));
+  __SET_VAR(data__->CTU0.,CU,,__GET_VAR(data__->R_TRIG1.Q,));
+  __SET_VAR(data__->CTU0.,R,,(__GET_VAR(data__->COUNTER_Q,) || __GET_VAR(data__->COUNTER_RESET,)));
+  __SET_VAR(data__->CTU0.,PV,,__GET_VAR(data__->COUNTER_MAX,));
+  CTU_body__(&data__->CTU0);
+  if (__GET_VAR(data__->CTU0.ENO,)) {
+    __SET_VAR(data__->,COUNTER_VALUE,,__GET_VAR(data__->CTU0.CV,));
+  };
+  __SET_VAR(data__->,COUNTER_Q,,__GET_VAR(data__->CTU0.Q,));
+  __SET_VAR(data__->TON1.,EN,,__BOOL_LITERAL(TRUE));
+  __SET_VAR(data__->TON1.,IN,,!(__GET_VAR(data__->PULSE,)));
+  __SET_VAR(data__->TON1.,PT,,__time_to_timespec(1, 500, 0, 0, 0, 0));
+  TON_body__(&data__->TON1);
+  __SET_VAR(data__->TOF0.,EN,,__GET_VAR(data__->TON1.ENO,));
+  __SET_VAR(data__->TOF0.,IN,,__GET_VAR(data__->TON1.Q,));
+  __SET_VAR(data__->TOF0.,PT,,__time_to_timespec(1, 500, 0, 0, 0, 0));
+  TOF_body__(&data__->TOF0);
+  __SET_VAR(data__->,PULSE,,__GET_VAR(data__->TOF0.Q,));
+
+  goto __end;
+
+__end:
+  return;
+} // WATERESP_body__() 
+
+
+
+
+
---
+++ build/POUS.h
@@ -0,0 +1,54 @@
+#include "beremiz.h"
+#ifndef __POUS_H
+#define __POUS_H
+
+#include "accessor.h"
+#include "iec_std_lib.h"
+
+__DECLARE_ENUMERATED_TYPE(LOGLEVEL,
+  LOGLEVEL__CRITICAL,
+  LOGLEVEL__WARNING,
+  LOGLEVEL__INFO,
+  LOGLEVEL__DEBUG
+)
+// FUNCTION_BLOCK LOGGER
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(BOOL,TRIG)
+  __DECLARE_VAR(STRING,MSG)
+  __DECLARE_VAR(LOGLEVEL,LEVEL)
+
+  // FB private variables - TEMP, private and located variables
+  __DECLARE_VAR(BOOL,TRIG0)
+
+} LOGGER;
+
+void LOGGER_init__(LOGGER *data__, BOOL retain);
+// Code part
+void LOGGER_body__(LOGGER *data__);
+// PROGRAM WATERESP
+// Data part
+typedef struct {
+  // PROGRAM Interface - IN, OUT, IN_OUT variables
+
+  // PROGRAM private variables - TEMP, private and located variables
+  __DECLARE_VAR(BOOL,PULSE)
+  __DECLARE_VAR(BOOL,COUNTER_ENABLE)
+  __DECLARE_VAR(BOOL,COUNTER_RESET)
+  __DECLARE_VAR(BOOL,COUNTER_Q)
+  __DECLARE_VAR(INT,COUNTER_MAX)
+  CTU CTU0;
+  __DECLARE_VAR(INT,COUNTER_VALUE)
+  TON TON1;
+  TOF TOF0;
+  R_TRIG R_TRIG1;
+
+} WATERESP;
+
+void WATERESP_init__(WATERESP *data__, BOOL retain);
+// Code part
+void WATERESP_body__(WATERESP *data__);
+#endif //__POUS_H
